8c4dc0d7d647befb9ff964f047fbc9a029521d56
Jesus Peralta <desarrolladorwebodm@gmail.com>
2025-12-30 15:18:16 -0600

    backend ready

---
+++ src/middleware/validation.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express'
+import { validationResult } from 'express-validator'
+
+export const handleInputErrors = (req: Request, res: Response, next: NextFunction ) => {
+        // Manejo de errores
+    let errors = validationResult(req)
+
+    console.log('Desde validations.ts');
+    
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+    next()
+}
